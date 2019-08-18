c1a870f18d644a675a1825e5fa5c222f8855194f
xdjzhh <xdjzhh@gmail.com>
2019-08-19 02:18:58 +1000

    new

---
+++ dynamic planning/动态规划回文子串.py
@@ -0,0 +1,51 @@
+'''
+
+    State:
+    dp[i][j]: 表示string[i...j]中的回文子序列长度
+
+    Initial State:
+    dp[i][i] == 1
+    string[i] == string[i+1] : dp[i][i+1] == 2
+    string[i] != string[i+1] : dp[i][i+1] == 1
+
+    State Transition:
+
+'''
+
+
+
+def solution(s):
+    dp = [[0] * len(s) for i in range(len(s))]
+    print(dp)
+    if not s:
+        return 0
+
+    n = len(s)
+
+
+    # init
+    for i in range(n):
+        dp[i][i] = 1  # length 1
+        if i < n-1:  # length 2
+            if s[i] == s[i +1]:
+                dp[i][i+1] = 2
+            else:
+                dp[i][i+1] = 1
+
+    print(dp)
+    for l in range(3,n+1):   #此处指的是子串长度范围从3开始    因为 1  2  在上循环已经初始化
+        for i in range(n-l+1):
+            j = i+l-1
+            # dp[i][j] = max([dp[i + 1][j], dp[i][j - 1]])
+            if s[i] == s[j]:
+                dp[i][j] = max([dp[i][j], dp[i + 1][j - 1] + 2])
+            else:
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+    print(dp)
+    return dp[0][n-1]
+    pass
+
+
+if __name__ == '__main__':
+    string = 'asddsaaaaaaaaaaaq'
+    print(solution(string))
